ca0818667f991abebfb3eb4669fe9db10994e433
amandadantasds <78170677+amandadantasds@users.noreply.github.com>
2021-03-19 01:36:39 -0300

    Delete LOGINCADASTRO directory

--- LOGINCADASTRO/verifica.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'conexao.php';
-if (isset($_SESSION['idUsuario']) && !empty($_SESSION ['idUsuario'])){}
-?>
--- LOGINCADASTRO/verificalogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(!$_SESSION['Email']) {
-	header('Location: login.php');
-	exit();
-}
-
-?>
